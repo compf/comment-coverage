60cb102da86aa2e5a2caf709e80c09ed5a65e0a7
Nils <nilsbaumgartner1994@gmail.com>
2021-07-17 17:15:18 +0200

    v1.10

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import core from '@actions/core';
-import github from '@actions/github';
+const core = require('@actions/core');
+const github = require('@actions/github');
 const execSync = require('child_process').execSync;
 
 console.log('Start Index');
